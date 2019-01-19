acbbe450828c39472c5e175f27fee50a41c58ce8
ckipp01 <christopherkipp0@gmail.com>
2019-01-19 14:01:50 +0100

    parsing json request and storing

--- api/log/index.js
+++ api/log/index.js
@@ -1,3 +1,13 @@
-module.exports = (req, res) => {
-  res.end('Welcome to Micro')
+const {json} = require('micro')
+const db = require('../../lib/db.js')
+
+module.exports = async (req, res) => {
+  try {
+  const log = await json(req)
+  const result = await db.storeLog(log)
+  return result 
+  } catch (err) {
+    console.log(err)
+    return 'something went wrong'
+  }
 }
