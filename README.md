404aa755cc21642a062d1a9d01a8be756f780f55
ckipp01 <christopherkipp0@gmail.com>
2019-03-13 21:57:12 +0100

    add in the GET option for logs with an optional amount parameter

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ándaga-core
 
-ándaga-core is the api and engine for my [ándaga](https://wiki.chronica.xyz/#andaga) time tracking system.
+ándaga-core is the api and core engine for my [ándaga](https://wiki.chronica.xyz/#andaga) time tracking system.
 
-The api is currently small and only has 3 routes defined.
+The api is currently small and only has 2 routes defined.
 
   - `GET /category` used to receive a list of all categories that have been used
+  - `GET /log` used to retrieve a certain amount of logs specified by an `amount` parameter
   - `POST /log` used for storing logs
-  - `GET /recall` used for recalling the last entry
 
 You can find out more about this project [here on my wiki](https://wiki.chronica.xyz/#andaga-core).
--- api/log/index.js
+++ api/log/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const url = require('url')
 const db = require('../../lib/db.js')
 const { Log } = require('../../lib/models/models.js')
 
@@ -8,22 +9,41 @@ module.exports = (req, res) => {
     res.writeHead(401, { 'Content-Type': 'text/plain' })
     res.end('Authentication required')
   } else {
-    let body = ''
-    req.on('data', chunk => {
-      body += chunk.toString()
-    })
-    req.on('end', () => {
-      try {
-        const log = new Log(JSON.parse(body))
-        db.storeLog(log)
-          .then(result => {
-            res.writeHead(200, { 'Content-Type': 'text/plain' })
-            res.end('Stored log successfully')
-          })
-      } catch (err) {
-        res.writeHead(400, { 'Content-Type': 'text/plain' })
-        res.end(err.message)
-      }
-    })
+    if (req.method === 'POST') {
+      let body = ''
+      req.on('data', chunk => {
+        body += chunk.toString()
+      })
+      req.on('end', () => {
+        try {
+          const log = new Log(JSON.parse(body))
+          db.storeLog(log)
+            .then(result => {
+              res.writeHead(200, { 'Content-Type': 'text/plain' })
+              res.end('Stored log successfully')
+            })
+        } catch (err) {
+          res.writeHead(400, { 'Content-Type': 'text/plain' })
+          res.end(err.message)
+        }
+      })
+    } else if (req.method === 'GET') {
+      const params = url.parse(req.url, true)
+      const amount = params.query.amount
+        ? params.query.amount
+        : 1
+      db.retrieveLogs(amount)
+        .then(result => {
+          res.writeHead(200, { 'Content-Type': 'application/json' })
+          res.end(JSON.stringify(result))
+        })
+        .catch(err => {
+          res.writeHead(400, { 'Content-Type': 'text/plain' })
+          res.end(err.message)
+        })
+    } else {
+      res.writeHead(405, { 'Content-Type': 'text/plain' })
+      res.end(req.method + ' is not allowed')
+    }
   }
 }
