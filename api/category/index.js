3029a805e15adec8682f5e4011c9f10899bcbdbc
ckipp01 <christopherkipp0@gmail.com>
2019-03-16 22:55:17 +0100

    add in logging for categories

--- api/category/index.js
+++ api/category/index.js
@@ -10,10 +10,12 @@ module.exports = (req, res) => {
     try {
       db.getCategories()
         .then(result => {
+          console.info('Successfully retrieved categories')
           res.writeHead(200, { 'Content-Type': 'application/json' })
           res.end(JSON.stringify(result))
         })
     } catch (err) {
+      console.err(err.message)
       res.writeHead(400, { 'Content-Type': 'text/plain' })
       res.end(err.message)
     }
