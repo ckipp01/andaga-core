dd19f80134c4c2e63403e6087e1b1143019e51e4
ckipp01 <christopherkipp0@gmail.com>
2019-03-20 21:15:40 +0100

    add in the ability to retrieve a list of projects as well

---
+++ api/projects/index.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const db = require('../../lib/db.js')
+
+module.exports = (req, res) => {
+  if (req.headers.authorization !== process.env.ANDAGA_AUTH) {
+    res.writeHead(401, { 'Content-Type': 'text/plain' })
+    res.end('Authentication required')
+  } else {
+    try {
+      db.getProjects()
+        .then(result => {
+          console.info('Successfully retrieved projects')
+          res.writeHead(200, { 'Content-Type': 'application/json' })
+          res.end(JSON.stringify(result))
+        })
+    } catch (err) {
+      console.err(err.message)
+      res.writeHead(400, { 'Content-Type': 'text/plain' })
+      res.end(err.message)
+    }
+  }
+}
